a7ec9fcac06fa3157fd2a7ac69ed938f83ada884
Geraldi Kusuma Arnanto <geral.sbi2@gmail.com>
2012-11-30 19:18:59 +0700

    update defconfig

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Fri Nov 30 18:54:44 CXT 2012"
+#define LINUX_COMPILE_TIME "18:54:44"
+#define LINUX_COMPILE_BY "aldi"
+#define LINUX_COMPILE_HOST "aldi-K43SV"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
